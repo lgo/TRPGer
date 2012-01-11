d48da0d90fd4bfe38a7b855837f26d538e6af0a8
Joey <legojoey17@gmail.com>
2012-01-10 22:31:39 -0500

    Format cleanup

--- src/thatproject/ThatProject.java
+++ src/thatproject/ThatProject.java
@@ -7,15 +7,14 @@ package thatproject;
 import thatproject.menu.MainMenu;
 
 /**
- *
+ * 
  * @author Joey
  */
 public class ThatProject {
 
-
     public static int x = 700, y = 600;
     public static MainMenu main = new MainMenu(x, y);
-    
+
     public static void main(String[] args) {
         main.start();
     }
--- src/thatproject/menu/MainMenu.java
+++ src/thatproject/menu/MainMenu.java
@@ -1,35 +1,33 @@
-
 package thatproject.menu;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
-public class MainMenu extends JFrame implements Runnable{
+public class MainMenu extends JFrame implements Runnable {
 
     public MainMenu(int x, int y) {
         setSize(x, y);
     }
-        
-    
-    
+
     @Override
     public void run() {
-        
+
     }
-    
+
     public void start() {
         setVisible(true);
-Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-int w = getSize().width;
-int h = getSize().height;
-int x = (dim.width-w)/2;
-int y = (dim.height-h)/2;
-setLocation(x, y);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int w = getSize().width;
+        int h = getSize().height;
+        int x = (dim.width - w) / 2;
+        int y = (dim.height - h) / 2;
+        setLocation(x, y);
     }
-    
+
     public void stop() {
         //
     }
-    
+
 }
