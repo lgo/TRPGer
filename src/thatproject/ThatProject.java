d0eb58d587de5cc47df8d39d667a26f31c3fe954
Joey <legojoey17@gmail.com>
2012-01-15 01:42:38 -0500

    Build 1 (Non-working)
    Get window sorted out, nearly ready for a working build!

--- src/thatproject/ThatProject.java
+++ src/thatproject/ThatProject.java
@@ -6,13 +6,14 @@ import thatproject.world.MapManager;
 
 public class ThatProject {
 
+    public static String name = "ThatProject";
     public static int x = 700, y = 600;
-    public static MainMenu main = new MainMenu(x, y);
+
     public static MapManager mm = new MapManager(0, 0, 100, 100);
 
     public static Save s = new Save();
 
     public static void main(String[] args) {
-        main.start();
+        MainMenu.exec();
     }
 }
--- src/thatproject/menu/MainMenu.java
+++ src/thatproject/menu/MainMenu.java
@@ -1,33 +1,72 @@
 package thatproject.menu;
 
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class MainMenu extends JFrame implements Runnable {
+import thatproject.ThatProject;
 
-    public MainMenu(int x, int y) {
-        setSize(x, y);
+public class MainMenu extends JPanel {
+
+    private static JTextField textField;
+    private static JTextArea textArea;
+
+    private static JDesktopPane desk = new JDesktopPane();
+
+    public MainMenu(JFrame frame) {
+        super(new GridBagLayout());
+        
+        //Setup text area
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBounds(10, 10, 400, 500);
+        
+        //Setup content pane
+        desk.setOpaque(false);
+        desk.add(textArea);
     }
 
-    @Override
-    public void run() {
+    /**
+     * Create the JFrame and add GUI contents
+     */
+    private static void init() {
+        JFrame frame = new JFrame(ThatProject.name);//Create new JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//End Java application on window close
 
-    }
+        MainMenu main = new MainMenu(frame);//Initialize un-static functions
+        frame.setSize(ThatProject.x, ThatProject.y);//Set frame size
+        frame.setVisible(true);
 
-    public void start() {
-        setVisible(true);
+        //Center the window
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        int w = getSize().width;
-        int h = getSize().height;
+        int w = frame.getSize().width;
+        int h = frame.getSize().height;
         int x = (dim.width - w) / 2;
         int y = (dim.height - h) / 2;
-        setLocation(x, y);
+        frame.setLocation(x, y);
+        
+        //Add content
+        frame.setContentPane(desk);
     }
 
-    public void stop() {
-        //
+    /**
+     * Start thread for creatingGUI
+     */
+    public static void exec() {
+        // Create a seperate scheduled dispatch thread for initializing GUI
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                init();
+            }
+        });
     }
-
 }
