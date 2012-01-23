771556cf9e694858864b82dbe606975d393fe88c
Joey <legojoey17@gmail.com>
2012-01-22 23:50:53 -0500

    Build #5 +Attack damage +Spider tutorial practically finished

--- src/thatproject/actions/Attack.java
+++ src/thatproject/actions/Attack.java
@@ -1,10 +1,16 @@
 package thatproject.actions;
 
+import java.util.Random;
+
 import thatproject.entities.being.Enemy;
+import thatproject.entities.being.Player;
 
 public class Attack {
 
     private static final String n = "\n";
+    private static Random generator = new Random();
+
+    public static final int SPACES = 20;
 
     public static String attackDisplay(int hp, int hpMax, int spaces, String name) {
         return name + n + "Health: " + healthDisplay(hp, hpMax, spaces);
@@ -17,7 +23,6 @@ public class Attack {
     public static String healthDisplay(int hp, int hpMax, int spaces) {
         double h = hp, hm = hpMax, s = spaces;
         String r = "[";
-        System.out.println(h / hm * s);
         for (int i = 0; i < spaces; i++) {
             if (i == 0 && hp > 0) {
                 r += "=";
@@ -29,5 +34,14 @@ public class Attack {
         }
         r += "] " + Integer.toString(hp) + "/" + Integer.toString(hpMax);
         return r;
+    }
+
+    public static int attack(Player p) {
+        return 0;
+
+    }
+
+    public static int attack(int low, int high) {
+        return generator.nextInt(high - low) + low;
     }
 }
--- src/thatproject/world/SpecialEvents.java
+++ src/thatproject/world/SpecialEvents.java
@@ -9,22 +9,61 @@ public class SpecialEvents {
     private static int stage = 0;
     private static boolean spider = false;
     private static int temp = 50;
+    private static String active;
 
     public static void tutorialSpider() {
         Commands.specialEvent = true;
         spider = true;
         if (stage == 0) {
-            MainMenu.set(Attack.attackDisplay(50, 50, 20, "Spider"));
+            MainMenu.set("Spider     <- This is the monsters name. Below is it's health points.\n");
+            MainMenu.add(Attack.healthDisplay(50, 50, 20));
+            MainMenu.addTemp("\n\nNow attack it by typing 'slash', one of the many attacks.");
             stage++;
         } else if (stage == 1) {
+            if (active.equals("slash")) {
+                int damage = Attack.attack(5, 10);
+                temp -= damage;
+                MainMenu.set(Attack.attackDisplay(temp, 50, Attack.SPACES, "Spider"));
+                MainMenu.add("\nYou slash away at the spider for " + damage + ".\n\n");
+                MainMenu.addTemp("Great! You got a nasty blow on the Spider. In this tutorial the spider won't actually attack back, for the sake of learning. Now if you'd go ahead and give it a good 'stab' to continue that would be great.");
+                stage++;
+            } else {
+                MainMenu.addTemp("\n\nYou didn't type 'slash'! Go ahead, try it out.");
+            }
         } else if (stage == 2) {
+            if (active.equals("stab")) {
+                int damage = Attack.attack(15, 25);
+                temp -= damage;
+                MainMenu.set(Attack.attackDisplay(temp, 50, Attack.SPACES, "Spider"));
+                MainMenu.add("\nYou give the spider deep impale for " + damage + ".\n\n");
+                MainMenu.addTemp("See how that did much more damage? That's because different attacks have different amounts of damage that it will induce. Go ahead and give the spider another 'stab'!");
+                stage++;
+            } else {
+                MainMenu.addTemp("If you would like to survive, I highly advise you give 'stab' a go.");
+            }
+            
+        } else if (stage == 3) {
+            if (active.equals("stab")) {
+                MainMenu.set(Attack.attackDisplay(temp, 50, Attack.SPACES, "Spider"));
+                MainMenu.add("\nYou lunge at the spider, but swiftly miss it with the dagger.\n\n");
+                MainMenu.addTemp("That's the downside of more powerful attacks such as stab, they miss more often then attacks such as slash.\nThroughout this journey you will find what suits you as well as what your limits are. With this in mind, finish off the Spider with what you know.");
+                stage++;
+            } else {
+                MainMenu.addTemp("If you really want to get going, you definitely need to give 'stab' another shot!");
+            }
+        }
+        
+        else if (temp > 0){
+            //Spider killing events
         } else {
+
             Commands.specialEvent = false;
             spider = false;
         }
     }
 
-    public static void command(String active) {
+    public static void command(String a) {
+        active = a;
         if (spider) {
             tutorialSpider();
         }
