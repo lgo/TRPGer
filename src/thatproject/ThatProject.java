e0f8944a10c205816dc5d14a0017b309eee0445b
Joey <legojoey17@gmail.com>
2012-01-23 19:12:49 -0500

    Adding pictures for inv

--- src/thatproject/ThatProject.java
+++ src/thatproject/ThatProject.java
@@ -4,19 +4,23 @@ import thatproject.menu.MainMenu;
 
 public class ThatProject {
 
+    //declaring variables and initializing their values.
     public static String name = "Absolute Power";
-    public static int x = 900, y = 600;
+    public static int x = 900, y = 600; //dimensions of game window
 
     public static MainMenu m;
 
     public static boolean threadFreeze = true;
 
+    //Main function which starts executes the main menu and starts the game.
     public static void main(String[] args) {
         try {
 
             MainMenu.exec();
             Game.start();
-        } catch (Exception e) {
+        } 
+        //Catches error and prints it.
+        catch (Exception e) { 
             e.printStackTrace();
         }
     }
