79a50f54f386189bcae45bf546e765bbd732b942
Sean <game_freak_9937@hotmail.com>
2012-01-23 19:07:43 -0500

    Not sure what's up with that properties one.

--- src/thatproject/Game.java
+++ src/thatproject/Game.java
@@ -12,6 +12,7 @@ import thatproject.world.World;
 
 public class Game {
 
+    //Declaring and Initializing public variables
     public static Player p;
     public static MapReader mapr;
     public static MonsterReader monsterr;
@@ -29,10 +30,12 @@ public class Game {
 
     public static boolean loaded = false;
 
+    //Initial starting function to starting function
     public static void start() {
-        init();
+        init(); //calls init() function
     }
 
+    //init() function calls initFiles(), creates a new Player and calls the load() function
     private static void init() {
         initFiles();
         p = new Player(Save.init());
@@ -42,11 +45,13 @@ public class Game {
         load();
 
     }
-
+    
+    //Calls intro() function
     private static void load() {
         intro();
     }
 
+    //Starts the readers for maps, zones, and others.
     private static void initFiles() {
         ZoneReader.exec();
         MapReader.exec();
@@ -55,10 +60,11 @@ public class Game {
         // AttackReader.exec();
     }
 
+    //Sets the intro text to the screen of the main menu when starting the game
     private static void intro() {
-        String n = ThatProject.name;
-        String nl = "\n";
-        String nl2 = "\n\n";
+        String n = ThatProject.name; //shorter than having to type the name every time
+        String nl = "\n"; //shorter than writing "\n" every time
+        String nl2 = "\n\n"; //again, shorter than putting "\n\n"
         world.startGame(gameStartX, gameStartY);
         MainMenu.set("Welcome to the world of " + n + "!" + nl + n + " is a text based RPG where you must make your way through the inner depths to succeed in your quest." + nl + "To play you will type commands and press enter in the field below." + nl2 + "For starting off, type yes to continue into the game, or no to exit the game." + nl2 + "You may also type help at any moment for a list of current usable commands and what they do. Alongside those are shorthand abbreviations for nearly all commands.");
 
