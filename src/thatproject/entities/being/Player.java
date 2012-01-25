abb545b9b72ff26ae90e1b6dec15d58c33c0522b
gameguy999 <game_freak_9937@hotmail.com>
2012-01-25 17:52:44 -0500

    :D

--- src/thatproject/entities/being/Player.java
+++ src/thatproject/entities/being/Player.java
@@ -7,11 +7,13 @@ import thatproject.util.Formulas;
 
 public class Player extends Entity {
 
+    //declaring and initializing variables
     public int hp, hpMax, stamina = 100, staminaMax = stamina;
     private int lvl, exp, str, end, dex, luc, gold;
     private int x, y;
     public int spareStats = 4;
 
+    //initializing renaining stats.
     public Player(int[] stats) {
         lvl = stats[0];
         exp = stats[1];
@@ -26,19 +28,23 @@ public class Player extends Entity {
         y = stats[10];
     }
 
+    //calculates damage to deal to enemy
     public int[] calculateDamage() {
         int temp = Formulas.attack(str);
         int[] temp2 = { (int) (temp * 0.7), temp };
         return temp2;
     }
 
+    //checks to see if exp required to level up has been reached
     public void checkXP() {
+        //reduces exp and levels up player when required exp reached.
         if (exp == 100 * Math.pow(1.15, lvl)) {
             exp -= 100 * Math.pow(1.15, lvl);
             levelUp();
         }
     }
 
+    //increases player level, gives stats points, and assigns/refreshes player's health value.
     public void levelUp() {
         lvl++;
         spareStats += 5;
@@ -66,19 +72,22 @@ public class Player extends Entity {
                 luc++;
                 break;
         }
-        spareStats--;
-        hpMax = 86 + str + end * 5 + lvl * 8;
-        MainMenu.refreshHP();
+        spareStats--; //lowers spare stat point used to increase a stat
+        hpMax = 86 + str + end * 5 + lvl * 8; //assigns new max health value
+        MainMenu.refreshHP(); //displays new max health value
     }
 
+    //Function for using a healing item
     public void heal(int stat) {
-        hp = hp + stat > hpMax ? hpMax : hp + stat;
-        MainMenu.refreshHP();
+        hp = hp + stat > hpMax ? hpMax : hp + stat; //Adds value to health and just sets to max health if increases by more than max
+        MainMenu.refreshHP(); //displays new health value
     }
 
+    //Function for taking damage from enemy
     public void hit(int damage) {
-        hp -= damage;
-        MainMenu.refreshHP();
+        hp -= damage; //reduces health
+        MainMenu.refreshHP(); //displays new health
+        //checks to see if you die, and if true calls gameover()
         if (hp <= 0) {
             Game.gameover();
         }
