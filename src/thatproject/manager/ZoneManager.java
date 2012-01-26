d1462c51b83e047b0253a3c2a4b54f9ed1dacf82
gameguy999 <game_freak_9937@hotmail.com>
2012-01-25 21:30:22 -0500

    PUSH!!!!!!!

--- src/thatproject/manager/ZoneManager.java
+++ src/thatproject/manager/ZoneManager.java
@@ -4,6 +4,7 @@ import thatproject.world.World;
 
 public class ZoneManager {
 
+    //Places monster in zone for spawning.
     public static void insertMonster(int z, int monster) {
         World.zones[z].insertMonster(monster);
     }
